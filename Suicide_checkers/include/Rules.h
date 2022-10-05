30ddd9ac669592887ef7dc620b0084ec1ecf5ec9
Allen1412 <phetololesego@gmail.com>
2022-10-05 05:22:02 +0200

    algorithms done

--- Suicide_checkers/include/Rules.h
+++ Suicide_checkers/include/Rules.h
@@ -5,7 +5,6 @@
 #ifndef RULES_H
 #define RULES_H
 #include <vector>
-#include <iostream>
 using namespace std;
 
 class Rules
@@ -13,17 +12,19 @@ class Rules
     public:
         Rules();
         ~Rules();
-        bool is_forbidden(int row, int col, vector <vector <char>> board);//checks if the destination is not a dot or not beyond boundaries
         bool is_jumping(int& row_main, int& col_main, vector<vector<char>>& board, int& player1, int& player2);//checks for a must-jump move
         bool is_on_boundaries(int row,int col, vector<vector<char>> board);// checks if player is at the other end of play
-        bool is_diagonal_up(int row1, int col1, int row2, int col2, vector<vector<char>> board);//move is diagonal going up
-        bool is_diagonal_down(int row1, int col1, int row2, int col2, vector<vector<char>> board);//move is diagonal going down
-        bool is_king(int row, int col, vector<vector<char>> board); // returns true if piece is crowned
+        bool can_move_left_up_diagonally(int row, int col,vector<vector<char>> board);// checks if it is possible to move is diagonal going up
+        bool can_move_left_down_diagonally(int row, int col,vector<vector<char>> board);// checks if it is possible to move is diagonal going up
+        bool can_move_right_up_diagonally(int row, int col,vector<vector<char>> board);// checks if it is possible to move is diagonal going up
+        bool can_move_right_down_diagonally(int row, int col,vector<vector<char>> board);// checks if it is possible to move is diagonal going up
         void make_king(int row, int col, vector<vector<char>>& board);//makes the king
 
     protected:
 
     private:
+         bool is_forbidden(int, int, vector <vector <char>>);//checks if the destination is not a dot or not beyond boundaries
+         bool is_king(int, int, vector<vector<char>>); // returns true if piece is crowned
 };
 
 #endif // RULES_H
--- Suicide_checkers/main.cpp
+++ Suicide_checkers/main.cpp
@@ -3,30 +3,53 @@
 #include <iostream>
 #include "Board.h"
 #include "Rules.h"
+#include <cmath>
+#include <cstdlib>
 
 using namespace std;
 
-void algorithm1(vector <vector<char>>& board, Board bd, Rules rules, int& player1, int& player2);
-void algorithm2(vector <vector<char>>& board, Board bd, Rules rules, int& player1, int& player2);
+// Random algorithm
+void algorithm1(vector <vector<char>>& board, Board& bd, Rules rules, int& player1, int& player2);
+//Non-random algorithm
+void algorithm2(vector <vector<char>>& board, Board& bd, Rules rules, int& player1, int& player2);
+// Function that forces a jump
 bool must_jump(char plyr, char plyr_king, vector <vector<char>>& board, Board bd, Rules rules, int& player1, int& player2);
+//Function that updates game variables
 void make_update(vector <vector<char>>& board, Board& bd, Rules& rules, int player1, int player2);
+//Function to find playable positions for either algorithm indicated by the character o for algorithm 1 and x for algorithm 2
+void find_playable_positions(vector<vector<char>>&, Rules rules, char alg, vector <int>&, vector <int>&, vector <int>&, vector <int>&, vector <int>&, vector <int>&);
+
+
+
+
+
+
 
 int main()
 {
     vector <vector <char>> board;
-    int sz = 6, player1, player2;
+    // Important coordinates tables
+    int sz , player1, player2, counter;
+
+
+
+    while (cin >> sz){
+
 
     Board bd (sz, player1, player2);
     Rules rules;
+    counter = 0;
 
     bd.initialize_board(board);
     bd.print_board(board);
-    int i = 0;
+    cout << endl;
 
-    while (bd.game_finished_state() == 0){
+
+
+     while (bd.game_finished_state() == 0){
 
         //play in alternating turns
-        if (i%2 == 0){
+        if (counter %2 == 0){
             algorithm1(board, bd, rules, player1, player2);
             make_update(board, bd, rules, player1, player2);
 
@@ -50,7 +73,11 @@ int main()
             break;
         }
 
-        i++;
+        counter++;
+    }
+
+
+
     }
 
 
@@ -59,63 +86,152 @@ int main()
 }
 
 
-void algorithm1(vector <vector<char>>& board, Board bd, Rules rules, int& player1, int& player2){
 
-    if(must_jump('o', 'O', board, bd, rules, player1, player2))
-        return;
-    int row1, col1, row2,col2;
-    cout << "o initial row position:" << endl;
-    cin >> row1;
-    cout << "o initial col position:" << endl;
-    cin >> col1;
 
-    cout << "o destination row position:" << endl;
-    cin >> row2;
-    cout << "o destination col position:" << endl;
-    cin >> col2;
 
-    if(board[row1][col1] == 'o')
-    {
-        board[row1][col1] = '#';
-        board[row2][col2] = 'o';
-        }
-     else  if(board[row1][col1] == 'O')
-    {
-        board[row1][col1] = '#';
-        board[row2][col2] = 'O';
-        }
 
-    bd.print_board(board);
-    cout << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+void algorithm1(vector <vector<char>>& board, Board& bd, Rules rules, int& player1, int& player2){
+
+    vector <int> initial_rows, initial_cols, destination_rows, destination_cols, one_dimension_initial, one_dimension_destination;
+    // character o represents algorithm 1
+    char alg = 'o';
+    find_playable_positions(board, rules, alg, initial_rows, initial_cols, destination_rows, destination_cols, one_dimension_initial, one_dimension_destination);
+    if(must_jump('o', 'O', board, bd, rules, player1, player2)){
+
+         return;
+
+    } else if(one_dimension_initial.size() >0){
+
+            // Randomness
+            srand(time(0));
+            int position = rand() % one_dimension_initial.size();
+
+            cout << "alg 1 from: " << one_dimension_initial.at(position)<< " to: " << one_dimension_destination.at(position)<< endl;
+
+
+
+            // random initial position
+            int row1 = initial_rows.at(position), col1 = initial_cols.at(position);
+            // random destination position
+            int row2 = destination_rows.at(position),col2 = destination_cols.at(position);
+
+            if(board[row1][col1] == 'o')
+            {
+                board[row1][col1] = '#';
+                board[row2][col2] = 'o';
+                if(rules.is_on_boundaries(row2,col2,board))
+                            rules.make_king(row2,col2,board);
+                }
+             else  if(board[row1][col1] == 'O')
+            {
+                board[row1][col1] = '#';
+                board[row2][col2] = 'O';
+                if(rules.is_on_boundaries(row2,col2,board))
+                            rules.make_king(row2,col2,board);
+                }
+
+            bd.print_board(board);
+            cout << endl << endl;
+
+    }else{
+                // if the previous conditions fails, then it is a draw since this one cannot move.
+                bd.update_game(3);
+            }
+
+
 
 }
-void algorithm2(vector <vector<char>>&board, Board bd, Rules rules, int& player1, int& player2){
 
-    if(must_jump('x', 'X', board, bd, rules, player1, player2))
-        return;
-    int row1, col1, row2,col2;
-    cout << "x initial row position:" << endl;
-    cin >> row1;
-    cout << "x initial col position:" << endl;
-    cin >> col1;
 
-    cout << "x destination row position:" << endl;
-    cin >> row2;
-    cout << "x destination col position:" << endl;
-    cin >> col2;
 
-    if(board[row1][col1] == 'x')
-    {
-        board[row1][col1] = '#';
-        board[row2][col2] = 'x';
-        }
-     else  if(board[row1][col1] == 'X')
-    {
-        board[row1][col1] = '#';
-        board[row2][col2] = 'X';
-        }
-    bd.print_board(board);
-    cout << endl;
+
+
+
+
+
+
+
+
+
+
+void algorithm2(vector <vector<char>>&board, Board& bd, Rules rules, int& player1, int& player2){
+
+    vector <int> initial_rows, initial_cols, destination_rows, destination_cols, one_dimension_initial, one_dimension_destination;
+    // character x represents algorithm 2 always
+    char alg = 'x';
+    find_playable_positions(board, rules, alg, initial_rows, initial_cols, destination_rows, destination_cols, one_dimension_initial, one_dimension_destination);
+    if(must_jump('x', 'X', board, bd, rules, player1, player2)){
+
+                return;
+
+    }else if (one_dimension_destination.size() > 0){
+
+
+
+            int diff = 100, temp1 = 100, temp2 = 100, position = 100;
+
+            // every possible destination column position is compared with the boundaries so that the nearest is chosen
+            for (int i = 0; i < one_dimension_initial.size(); i++){
+
+
+
+                temp1 = destination_rows.at(i) - 0;
+                temp2 = (one_dimension_initial.size() -1 ) - destination_rows.at(i);
+
+                if((temp1<=temp2) && (temp1 <= diff)){
+                    diff = temp1;
+                    position = i;
+                }else if ((temp2<=temp1) && (temp2 <= diff)){
+                    diff = temp1;
+                    position = i;
+                }
+
+            }
+
+            //initial coordinates
+            int row1 = initial_rows.at(position), col1 = initial_cols.at(position);
+
+
+            //final coordinates
+            int row2 = destination_rows.at(position),col2 = destination_cols.at(position);
+
+            if(board[row1][col1] == 'x')
+            {
+                board[row1][col1] = '#';
+                board[row2][col2] = 'x';
+                if(rules.is_on_boundaries(row2,col2,board))
+                            rules.make_king(row2,col2,board);
+                }
+             else  if(board[row1][col1] == 'X')
+            {
+                board[row1][col1] = '#';
+                board[row2][col2] = 'X';
+                if(rules.is_on_boundaries(row2,col2,board))
+                            rules.make_king(row2,col2,board);
+                }
+
+            cout << "alg 2 from: " << one_dimension_initial.at(position) << " To :" << one_dimension_destination.at(position) << endl;
+            bd.print_board(board);
+            cout << endl << endl;
+            }
+
+            else{
+                // if the previous conditions fails, then it is a draw since this one cannot move.
+                bd.update_game(3);
+            }
+
 
 }
 
@@ -141,7 +257,7 @@ bool must_jump(char plyr, char plyr_king, vector <vector<char>>& board, Board bd
                     }
                 if(jumps>0){
                    bd.print_board(board);
-                   cout << endl;
+                   cout << endl << endl;
                    return true;
 
                 }
@@ -153,12 +269,192 @@ bool must_jump(char plyr, char plyr_king, vector <vector<char>>& board, Board bd
 }
 
 
+
+
+
+
+
 void make_update(vector <vector<char>>& board, Board& bd, Rules& rules, int player1, int player2){
 
     if(player1 == 0)
         bd.update_game(1);
     else if(player2 == 0)
         bd.update_game(2);
+
+}
+
+
+
+
+
+
+
+void find_playable_positions(vector<vector<char>>& board,Rules rules,char alg, vector <int>& initial_rows, vector <int>& initial_cols, vector <int>& destination_rows, vector <int>& destination_cols, vector <int>& one_dimension_initial, vector <int>& one_dimension_destination)
+
+{
+       int initial = 0;
+       int finl = 0;
+       int quotient = board.size()/2;
+
+       for (int i = 0; i < board.size(); i++){
+            for(int j =0; j< board.size(); j++){
+
+
+                 if(alg == 'x'){
+                         if (board[i][j] == 'x' || board[i][j] == 'X'){
+                            if(rules.can_move_left_up_diagonally(i, j, board)){
+
+                                initial_rows.push_back(i);
+                                initial_cols.push_back(j);
+                                destination_rows.push_back(i-1);
+                                destination_cols.push_back(j-1);
+
+                                // exuation to convert the 2D coordinates to 1D
+                                initial = (i* quotient) + floor(j/2) + 1;
+                                finl = (i-1)*(quotient) + floor((j-1)/2) + 1;
+
+                                one_dimension_initial.push_back(initial);
+                                one_dimension_destination.push_back(finl);
+
+                            }
+                            if(rules.can_move_right_up_diagonally(i, j, board)){
+
+                                initial_rows.push_back(i);
+                                initial_cols.push_back(j);
+                                destination_rows.push_back(i-1);
+                                destination_cols.push_back(j+1);
+
+                                // exuation to convert the 2D coordinates to 1D
+                                initial = (i*quotient) + floor(j/2) + 1;
+                                finl = (i-1)*(quotient) + floor((j+1)/2) + 1;
+
+                                one_dimension_initial.push_back(initial);
+                                one_dimension_destination.push_back(finl);
+                            }
+                } else if(board[i][j] == 'X'){
+
+
+                        if(rules.can_move_left_down_diagonally(i, j, board)){
+
+                                initial_rows.push_back(i);
+                                initial_cols.push_back(j);
+                                destination_rows.push_back(i+1);
+                                destination_cols.push_back(j-1);
+
+                                // exuation to convert the 2D coordinates to 1D
+                                initial = (i*quotient) + floor(j/2) + 1;
+                                finl = ((i+1)*(quotient)) + floor((j-1)/2) + 1;
+
+                                one_dimension_initial.push_back(initial);
+                                one_dimension_destination.push_back(finl);
+
+                            }
+                            if(rules.can_move_right_down_diagonally(i, j, board)){
+
+                                initial_rows.push_back(i);
+                                initial_cols.push_back(j);
+                                destination_rows.push_back(i+1);
+                                destination_cols.push_back(j+1);
+
+                                // exuation to convert the 2D coordinates to 1D
+                                initial = (i*quotient) + floor(j/2) + 1;
+                                finl = (i+1)*(quotient) + floor((j+1)/2) + 1;
+
+                                one_dimension_initial.push_back(initial);
+                                one_dimension_destination.push_back(finl);
+
+                            }
+
+
+                }
+
+                }
+
+
+                //Player 2 only moves down diagonally, either left or right, kings also move in these directions
+                else if(alg == 'o')
+
+                {
+
+
+                    if (board[i][j] == 'o' ||  board[i][j] == 'O'){
+
+                            if(rules.can_move_left_down_diagonally(i, j, board)){
+
+                                initial_rows.push_back(i);
+                                initial_cols.push_back(j);
+                                destination_rows.push_back(i+1);
+                                destination_cols.push_back(j-1);
+
+                                // exuation to convert the 2D coordinates to 1D
+                                initial = (i*quotient) + floor(j/2) + 1;
+                                finl = ((i+1)*(quotient)) + floor((j-1)/2) + 1;
+
+                                one_dimension_initial.push_back(initial);
+                                one_dimension_destination.push_back(finl);
+
+                            }
+                            if(rules.can_move_right_down_diagonally(i, j, board)){
+
+                                initial_rows.push_back(i);
+                                initial_cols.push_back(j);
+                                destination_rows.push_back(i+1);
+                                destination_cols.push_back(j+1);
+
+                                // exuation to convert the 2D coordinates to 1D
+                                initial = (i*quotient) + floor(j/2) + 1;
+                                finl = (i+1)*(quotient) + floor((j+1)/2) + 1;
+
+                                one_dimension_initial.push_back(initial);
+                                one_dimension_destination.push_back(finl);
+
+                            }
+
+                }else if(board[i][j] == 'O'){
+
+
+                    if(rules.can_move_left_up_diagonally(i, j, board)){
+
+                        initial_rows.push_back(i);
+                        initial_cols.push_back(j);
+                        destination_rows.push_back(i-1);
+                        destination_cols.push_back(j-1);
+
+                        // exuation to convert the 2D coordinates to 1D
+                        initial = (i* quotient) + floor(j/2) + 1;
+                        finl = (i-1)*(quotient) + floor((j-1)/2) + 1;
+
+                        one_dimension_initial.push_back(initial);
+                        one_dimension_destination.push_back(finl);
+
+                    }
+                    if(rules.can_move_right_up_diagonally(i, j, board)){
+
+                        initial_rows.push_back(i);
+                        initial_cols.push_back(j);
+                        destination_rows.push_back(i-1);
+                        destination_cols.push_back(j+1);
+
+                        // exuation to convert the 2D coordinates to 1D
+                        initial = (i*quotient) + floor(j/2) + 1;
+                        finl = (i-1)*(quotient) + floor((j+1)/2) + 1;
+
+                        one_dimension_initial.push_back(initial);
+                        one_dimension_destination.push_back(finl);
+                    }
+
+
+                }
+
+
+
+                }
+
+
+            }
+       }
+
+
 
 }
 
